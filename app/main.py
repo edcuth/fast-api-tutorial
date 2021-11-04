61d5d0e03606b211cfec22b190da57276618821d
edcuth <38770919+edcuth@users.noreply.github.com>
2021-11-04 20:12:15 -0300

    add CRUD support through ORM, move schema to new file, create a PostBase to use for future schema

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
+from os import stat
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
 from sqlalchemy import engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models
+from . import models, schema
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -14,12 +14,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 #run uvicorn main:app --reload to start the web server
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    id: int = None
 
 while True:
     try:
@@ -45,7 +39,7 @@ async def get_posts():
 
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post):
+async def create_post(post: schema.PostCreate):
     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
     new_post = cursor.fetchone()
     conn.commit()
@@ -70,7 +64,7 @@ async def delete_post(id: int):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put('/posts/{id}')
-async def update_post(id: int, post: Post):
+async def update_post(id: int, post: schema.PostCreate):
     cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, id))
     post = cursor.fetchone()
     conn.commit()
@@ -78,6 +72,42 @@ async def update_post(id: int, post: Post):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
     return {"message":f"post {id} updated", "post":post}
 
-@app.get("/sqlalchemy")
-async def test_posts(db: Session = Depends(get_db)):
-    return {"status": "success"}
+@app.get("/sqlalchemy/posts")
+async def orm_get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
+
+@app.post("/sqlalchemy/posts", status_code=status.HTTP_201_CREATED)
+def orm_create_post(post: schema.PostCreate, db: session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {'new post': new_post}
+
+
+@app.get("/sqlalchemy/posts/{id}")
+async def orm_get_post( id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} not found')
+    return {'post': post}
+
+@app.delete('/sqlalchemy/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def orm_delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} not found')
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/sqlalchemy/posts/{id}")
+async def orm_update_post(id: int, updated_post: schema.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} not found')
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return {'updated post': post_query.first()}
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from sqlalchemy.engine import base
 from sqlalchemy.sql.schema import Column
 from .database import Base
 from sqlalchemy import column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Post(Base):
     __tablename__ = "posts"
@@ -9,5 +11,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True, nullable=True)
-
+    published = Column(Boolean, server_default='TRUE', nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
